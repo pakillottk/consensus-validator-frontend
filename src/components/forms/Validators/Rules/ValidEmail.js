848105755d9a9682b649be11d1862c4416578f40
Francisco Gázquez <pmgmsc@gmail.com>
2018-05-02 19:58:38 +0200

    OPTIMIZATION in ValidEmail rule (was creating regex on every call)

--- src/components/forms/Validators/Rules/ValidEmail.js
+++ src/components/forms/Validators/Rules/ValidEmail.js
@@ -1,9 +1,10 @@
 import BaseRule from './BaseRule';
 
+const mailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 export default class ValidEmail extends BaseRule {
     evaluation( value ) {
-        const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if( !regex.test( value ) ) {
+        if( !mailRegex.test( value ) ) {
             return 'Email no válido';
         }
         
