85088452e8c0c1bbe28e0ca8d02825b523b0f781
Francisco Gázquez <pmgmsc@gmail.com>
2018-05-10 13:12:13 +0200

    IMPROVED speed by precaching in browser the session imgs

--- src/redux/reducers/sessions.js
+++ src/redux/reducers/sessions.js
@@ -1,2 +1,15 @@
 import EntityReducer from './generators/EntityReducer'
-export default EntityReducer( 'Sessions', {} );
+import API from '../../API/API'
+
+//Make imgs available in browser cache for better speed
+const preprocessor = ( data ) => {
+    if( data.header_img ) {
+        (new Image()).src = API.getFullPath(data.header_img);
+    }
+    if( data.logos_img ) {
+        (new Image()).src = API.getFullPath(data.logos_img);
+    }
+    return data;
+}
+
+export default EntityReducer( 'Sessions', {}, preprocessor );
